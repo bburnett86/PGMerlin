e98d70dc40078402d21f8187ad822340bd5a0fe6
bburnett86 <breton.burnett@gmail.com>
2016-08-01 20:19:55 -0500

    Fixed pages that were AJAXing layouts

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :city_states, only: [:new, :create, :show]
 
-  resources :zipcode, only: [:new, :create, :show]
+  resources :zipcodes, only: [:new, :create, :show]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
